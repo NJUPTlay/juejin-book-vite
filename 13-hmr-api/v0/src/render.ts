16f662339a0cb8c738295a0d89e8308408482d9d
2782522198@qq.com <2782522198@qq.com>
2024-08-06 22:19:29 +0800

    feat: 接收自身更新

--- 13-hmr-api/v0/src/render.ts
+++ 13-hmr-api/v0/src/render.ts
@@ -1,5 +1,13 @@
 import './style.css'
 
+//即使是修改注释也会导致整个页面的全部渲染
+//热更新的机制，Vite开发服务器使用Node.js的文件系统的API（fs.watch）或者其他的监听工具，监控文件的更改。检测到相应的变化就通过websocket推送消息到浏览器，浏览器收到通知，会进行相应的操作。
+if(import.meta.hot){
+  //仅在开发模式下 也只会在。生产模式下会被删除代码优化
+  import.meta.hot.accept((mod)=>{mod.render()})
+  
+}
+
 export const render = () => {
   const app = document.querySelector<HTMLDivElement>('#app')!
   app.innerHTML = `
